12b92835496a11e7d6be3c9051f6ab968035f995
beller0ph0nt <kras_den@rambler.ru>
2015-04-12 17:57:27 +0700

    В пакет добавлена лицензия

--- create_game.bat
+++ create_game.bat
@@ -6,7 +6,9 @@ cd .\win
 copy /b "C:\Program Files\LOVE\love.exe" + ScalperTest.love ScalperTest.exe
 del ScalperTest.love
 copy "C:\Program Files\LOVE\*.dll" .
+copy "C:\Program Files\LOVE\license.txt" .
 zip -9 -q -r ScalperTest.zip .
 del *.exe
 del *.dll
+del *.txt
 cd .\..
